8a260f578325055b1afee1e26d7167401aa7653b
sagarjauhriAlgo <sagar.jauhri@algoscale.com>
2022-01-24 12:01:58 +0530

    refactor(step-components): now steps are available under the component folder.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -10,7 +11,9 @@ import { MaterialModule } from './material.module';
     MaterialModule
   ],
   exports: [
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class SharedModule { }
--- src/app/stepper-arch/stepper-arch.module.ts
+++ src/app/stepper-arch/stepper-arch.module.ts
@@ -4,10 +4,10 @@ import { CommonModule } from '@angular/common';
 import { StepperArchRoutingModule } from './stepper-arch-routing.module';
 import { StepperArchComponent } from './stepper-arch.component';
 import { SharedModule } from '../shared/shared.module';
-import { Step1Component } from './step1/step1.component';
-import { Step2Component } from './step2/step2.component';
-import { Step3Component } from './step3/step3.component';
-import { Step4Component } from './step4/step4.component';
+import { Step1Component } from './components/step1/step1.component';
+import { Step2Component } from './components/step2/step2.component';
+import { Step3Component } from './components/step3/step3.component';
+import { Step4Component } from './components/step4/step4.component';
 
 
 @NgModule({
